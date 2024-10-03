e91eb0d2812e9b817c08ec7b7dba4541a9fdf90b
Chus <mjesus.mh.12@gmail.com>
2024-10-03 10:49:45 +0200

    Ejr.: 26 completado

--- src/main/java/com/campusdual/classroom/Contact.java
+++ src/main/java/com/campusdual/classroom/Contact.java
@@ -1,5 +1,84 @@
 package com.campusdual.classroom;
 
-public class Contact{
+import java.text.Normalizer;
 
+public class Contact implements ICallActions {
+
+    private String name;
+    private String surnames;
+    private String phone;
+    private String code;
+
+    public Contact(String name, String surnames, String phone) {
+        this.name = name;
+        this.surnames = surnames;
+        this.phone = phone;
+        this.code = generateCode(name, surnames);
+
+    }
+
+    private String generateCode(String name, String surnames) {
+        String normalizedSurname = Normalizer.normalize(surnames.toLowerCase(), Normalizer.Form.NFD)
+                .replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+
+        String normalizedName = Normalizer.normalize(name.toLowerCase(), Normalizer.Form.NFD)
+                .replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+
+        System.out.println("Normalized Name: " + normalizedName);
+        System.out.println("Normalized Surname: " + normalizedSurname);
+
+        String initialName = normalizedName.substring(0, 1);
+        String[] surnameParts = normalizedSurname.split(" ");
+
+        if (surnameParts.length == 1) {
+            return initialName + surnameParts[0];
+
+        }
+
+        String firstSurnameInitial = surnameParts[0].substring(0, 1);
+        StringBuilder code = new StringBuilder(initialName + firstSurnameInitial);
+
+        for (int i = 1; i < surnameParts.length; i++) {
+            code.append(surnameParts[i]);
+        }
+
+        return code.toString();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurnames() {
+        return surnames;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public void callMyNumber() {
+        System.out.println("Llamandome a mi mismo, " + name + " " + surnames + ", al teléfono " + phone);
+    }
+
+    @Override
+    public void callOtherNumber(String number) {
+        System.out.println("Llamando a " + name + " " + surnames + " desde " + number);
+    }
+
+    @Override
+    public void showContactDetails() {
+
+        System.out.println("Name: " + name);
+        System.out.println("Surnames: " + surnames);
+        System.out.println("Phone: " + phone);
+        System.out.println("Code: " + code);
+
+    }
 }
--- src/main/java/com/campusdual/classroom/Phonebook.java
+++ src/main/java/com/campusdual/classroom/Phonebook.java
@@ -1,5 +1,98 @@
 package com.campusdual.classroom;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Phonebook {
+
+    private Map<String, Contact> data = new HashMap<>();
+
+    public Map<String, Contact> getData() {
+        return data;
+    }
+
+    public void addContact(Contact contact) {
+
+        data.put(contact.getCode(), contact);
+        System.out.println("Contacto añadido: " + contact.getName() + " (" + contact.getCode() + ")\n");
+
+    }
+
+    public void deleteContact(String code) {
+
+        Contact removed = data.remove(code);
+        if (removed != null) {
+            System.out.println("Contacto eliminado: " + removed.getName() + "\n");
+        } else {
+            System.out.println("Contacto no encontrado.\n");
+        }
+
+    }
+
+    public void showPhonebook() {
+
+        if (data.isEmpty()) {
+            System.out.println("Phonebook esta vacio.\n");
+        } else {
+            for (Contact contact : data.values()) {
+
+                contact.showContactDetails();
+                System.out.println("---------------------\n");
+
+            }
+        }
+
+    }
+
+    public void showMenu() {
+
+        Scanner scanner = new Scanner(System.in);
+        int option;
+
+        do {
+            System.out.println("Menu Phonebook:\n");
+            System.out.println("1. Añadir contacto");
+            System.out.println("2. Listar contactos");
+            System.out.println("3. Borrar contacto");
+            System.out.println("4. Salir");
+            System.out.print("Elige una opcion: ");
+            option = scanner.nextInt();
+            System.out.print("\n");
+            scanner.nextLine();
+
+            switch (option) {
+
+                case 1:
+                    System.out.print("Añada nombre: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Añada apellidos: ");
+                    String surnames = scanner.nextLine();
+                    System.out.print("Añada número de teléfono: ");
+                    String phone = scanner.nextLine();
+                    addContact(new Contact(name, surnames, phone));
+                    break;
+
+                case 2:
+                    showPhonebook();
+                    break;
+
+                case 3:
+                    System.out.print("Inserte un contacto para borrarlo: ");
+                    String code = scanner.nextLine();
+                    deleteContact(code);
+                    break;
+
+                case 4:
+                    System.out.println("Saliendo....\n");
+                    break;
+
+                default:
+                    System.out.println("Opción no valida, pruebe otra vez.\n");
+            }
+
+        } while (option != 4);
+
+    }
 
 }
